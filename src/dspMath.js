98814baf04ab8138a2140f2087dc2de1ac5f1ae9
Enzuo <potc.zon.e+git@gmail.com>
2021-02-20 00:27:09 +0800

    dspMath use new recipe data structure to calculate production chain

--- src/dspMath.js
+++ src/dspMath.js
@@ -1,33 +1,40 @@
 const materials = require('./data/materials.json')
 const factories = require('./data/factories.json')
+const RECIPES   = require('./data/recipes.json')
 
 
 /**
  * return all materials needed for the production of this material
  * @param {String} material name
- * @param {Number} qty per second
+ * @param {Number} qtyNeededPerSecond
  * @param {Array} chain production chain array
  * @param {Integer} depth
  */
-function computeProductionChain(material, qty, chain, depth) {
+function computeProductionChain(material, qtyNeededPerSecond, chain, depth) {
   chain = chain ? chain : []
   depth = depth ? depth + 1 : 1
-  qty = qty ? qty : 0
+  var qty = qtyNeededPerSecond ? qtyNeededPerSecond : 0
 
   if (depth > 20) return 
   
-  material = getMaterialDetails(material)
-  material.qty = qty
-  material.depth = depth
+  var materialObj = getMaterialDetails(material)
+  materialObj.name = material
+  materialObj.qty = qty
+  materialObj.depth = depth
 
-  chain.push(material)
+  chain.push(materialObj)
 
-  if(material.recipes){
-    var recipe = material.recipes[0]
+  var {recipe, materialIndex} = findRecipe(material)
+
+  if (recipe) {
+    materialObj.recipe = recipe
+    var qtyMadePerRecipe = recipe.output[materialIndex][0]
+    var qtyMadePerSecondPerRecipe = qtyMadePerRecipe / recipe.time
+    var qtyRecipeNeededPerSecond = qtyNeededPerSecond / qtyMadePerSecondPerRecipe
   
-    for (var i=0; i < recipe.materials.length; i++) {
-      var neededMaterial = recipe.materials[i][1]
-      var neededQuantity = recipe.materials[i][0] * qty
+    for (var i=0; i < recipe.input.length; i++) {
+      var neededMaterial = recipe.input[i][1]
+      var neededQuantity = recipe.input[i][0] * qtyRecipeNeededPerSecond
   
       computeProductionChain(neededMaterial, neededQuantity, chain, depth)
     }
@@ -51,6 +58,24 @@ function getMaterialDetails(materialName) {
     return { name : materialName }
   }
   return clone(material)
+}
+
+function findRecipe (material) {
+  var materialIndex
+  var recipe = RECIPES.find(function(r){
+    for(var j=0; j < r.output.length; j++) {
+      if(r.output[j][1] === material){
+        materialIndex = j
+        return true
+      }
+    }
+  });
+
+  if(!recipe){
+    return {recipe: null}
+  }
+
+  return {recipe, materialIndex}
 }
 
 function mergeProductionChain(chain) {
