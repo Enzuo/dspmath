6fc6009ff869131dc0e56b601e34d4ec3a0b8a67
Enzuo <potc.zon.e+git@gmail.com>
2021-03-14 02:30:42 +0800

    Fix drag smoothness
    Thanks here https://stackoverflow.com/questions/13078535/stuttering-drag-when-using-d3-behavior-drag-and-transform

--- src/d3mall.js
+++ src/d3mall.js
@@ -43,6 +43,11 @@ function renderGraph (svg, data2) {
       .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")");
 
+  // Init force simulation
+  const simulation = d3.forceSimulation(data.nodes)
+    .force("link", d3.forceLink(data.links).id(d => d.id))
+    .force("charge", d3.forceManyBody().strength(-200))
+    .force("center", d3.forceCenter(width / 2, height / 2));
 
   // Initialize the links
   var link = svg
@@ -58,14 +63,11 @@ function renderGraph (svg, data2) {
     .data(data.nodes)
     .enter()
     .append("g")
+    .call(drag(simulation));
 
   node.append("circle")
     .attr("r", 20)
     .style("fill", function(d) { return d.type === 'building' ? '#00bbff' : '#003333' })
-    .call(d3.drag()
-    .on("start", dragstarted)
-    .on("drag", dragged)
-    .on("end", dragended));
 
   node.append("title")
     .text(function(d) { return d.id; });
@@ -86,16 +88,26 @@ function renderGraph (svg, data2) {
     .attr("clip-path",d => 'url(#'+d.id.replaceAll(' ', '')+')')
     .attr("pointer-events","none")
 
+  // var force = d3.layout.force()
+  //   .gravity(0.05)
+  //   .distance(100)
+  //   .charge(-100)
+  //   .size([width, height]);
+
   // Let's list the force we wanna apply on the network
-  var simulation = d3.forceSimulation(data.nodes)                 // Force algorithm is applied to data.nodes
-      .force("link", d3.forceLink()                               // This force provides links between nodes
-            .id(function(d) { return d.id; })                     // This provide  the id of a node
-            .links(data.links)                                    // and this the list of links
-      )
-      .force("charge", d3.forceManyBody().strength(-200))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
-      .force("center", d3.forceCenter(width / 2, height / 2))     // This force attracts nodes to the center of the svg area
-      // .on("end", ticked);
-      .on("tick", ticked);
+  // var simulation = d3.forceSimulation(data.nodes)                 // Force algorithm is applied to data.nodes
+  //     .force("link", d3.forceLink()                               // This force provides links between nodes
+  //           .id(function(d) { return d.id; })                     // This provide  the id of a node
+  //           .links(data.links)                                    // and this the list of links
+  //     )
+  //     .force("charge", d3.forceManyBody().strength(-200))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
+  //     .force("center", d3.forceCenter(width / 2, height / 2))     // This force attracts nodes to the center of the svg area
+  //     // .on("end", ticked);
+  //     .on("tick", ticked);
+
+
+  simulation
+      .on("tick", ticked)
 
   // This function is run at each iteration of the force algorithm, updating the nodes position.
   function ticked() {
@@ -113,22 +125,31 @@ function renderGraph (svg, data2) {
       })
   }
 
-  function dragstarted(event, d) {
+}
+
+const drag = simulation => {
+  
+  function dragstarted(event) {
     if (!event.active) simulation.alphaTarget(0.3).restart();
-    d.fx = d.x;
-    d.fy = d.y;
+    event.subject.fx = event.subject.x;
+    event.subject.fy = event.subject.y;
   }
   
-  function dragged(event, d) {
-    d.fx = event.x;
-    d.fy = event.y;
+  function dragged(event) {
+    event.subject.fx = event.x;
+    event.subject.fy = event.y;
   }
   
-  function dragended(event, d) {
+  function dragended(event) {
     if (!event.active) simulation.alphaTarget(0);
-    d.fx = null;
-    d.fy = null;
+    event.subject.fx = null;
+    event.subject.fy = null;
   }
+  
+  return d3.drag()
+      .on("start", dragstarted)
+      .on("drag", dragged)
+      .on("end", dragended);
 }
 
 
